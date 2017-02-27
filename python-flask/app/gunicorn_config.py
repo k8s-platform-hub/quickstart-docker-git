1f57abd85d8efc5e70df4b552d908b96385438c5
Arvi Shankar <aravind@34cross.in>
2017-02-27 16:03:31 +0530

    added default port for flask app

--- python-flask/app/gunicorn_config.py
+++ python-flask/app/gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
 import multiprocessing
 
-bind = "0.0.0.0:" + os.environ["APP_PORT"]
+bind = "0.0.0.0:" + os.environ.get("APP_PORTS", "8080")
 workers = (multiprocessing.cpu_count() * 2) + 1
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
